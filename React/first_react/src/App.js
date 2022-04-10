d718945a1d7b6836da6dfbb77d790abf388d0406
c-vanderhoof <christian.vanderhoof@gmail.com>
2022-04-09 21:58:12 -0400

    Init Commit

---
+++ React/first_react/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello Dojo!</h1>
+        <h2>Things I need to do: </h2>
+        <p>- Walk the dog</p>
+        <p>- Learn React</p>
+        <p>- Eat dinner</p>
+        <p>- Go to work</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
