22ae6418eb0d068b39bd625b013d322a3acde297
c-vanderhoof <christian.vanderhoof@gmail.com>
2022-04-12 23:43:10 -0400

    Edited for Putting It Together core assignment

--- React/prop_it_up/src/components/PersonCard.js
+++ React/prop_it_up/src/components/PersonCard.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 
 const PersonCard = (props) => {
     const { personcard } = props;
     const {firstName, lastName, age, hairColor} = personcard;
+    const [stateAge,setStateAge] = useState(age);
     return ( 
         <div>
             <h1>{ lastName },{firstName }</h1>
-            <p>Age:{age}</p>
+            <p>Age:{stateAge}</p>
             <p>Hair Color: {hairColor}</p>
+            <button onClick={()=> setStateAge(stateAge + 1)}>It's My Birthday!!</button>
         </div>
         
     );
-};
+}
 
 export default PersonCard;
