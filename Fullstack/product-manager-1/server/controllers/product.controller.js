18f938e061718f6dc481daf681fd9be02e2feb33
c-vanderhoof <christian.vanderhoof@gmail.com>
2022-05-15 13:53:42 -0400

    Product Manager Part 3

--- Fullstack/product-manager-1/server/controllers/product.controller.js
+++ Fullstack/product-manager-1/server/controllers/product.controller.js
@@ -18,5 +18,20 @@ module.exports = {
         .then((newProduct) => res.json(newProduct))
         .catch((err) => console.log(err));
     }, 
+
+    updateProduct: (req, res) => {
+        Product.findByIdAndUpdate({_id: req.params.id}, req.body, {
+            new:true,
+            runValidators:true,
+        })
+        .then((updatedProduct) => res.json(updatedProduct))
+        .catch((err) => console.log(err));
+    }, 
+
+    deleteProduct: (req, res) => {
+        Product.deleteOne({_id: req.params.id})
+            .then((deletedId)=> res.json(deletedId))
+            .catch ((err) => console.log(err));
+    },
 };
 
